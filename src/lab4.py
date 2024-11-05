4a93ab6707721d7604f6ff97318ad05553137488
User <user@Air-2017.Dlink>
2024-11-05 19:13:59 +0200

    fix code in lab4 with Insect class

--- src/lab4.py
+++ src/lab4.py
@@ -1,15 +1,13 @@
 class Insect:
-#конструктор
+#constructor
     def __init__(self, name = None, speed = None, mass = None, about = '', living = 0):
         self.__name = name
         self.__speed = speed
         self.__mass = mass
         self.about = about
         self.living = living
-#деструктор
-    def __del__(self):
-        print("Insect end")
-#методи доступу
+
+#access methods
     def get_name(self):
         return self.__name
 
@@ -28,7 +26,9 @@ class Insect:
     def set_mass(self, new_mass):
         self.__mass = new_mass
 
-#перевизначення str i repr
+    def __del__(self):
+        print("Insect end")
+
     def __str__(self):
         return f"Insect: name: {self.__name}, speed: {self.__speed} m/s, mass: {self.__mass} g"
 
@@ -36,8 +36,7 @@ class Insect:
         return (f"Insect(name='{self.__name}', speed={self.__speed}, mass={self.__mass}, "
                 f"about='{self.about}', living={self.living})")
 
-#використовується щоб визначити чи виконується цей скрипт як основний
-# if __name__ == "__main__":
+# is used to determine whether this script is executed as the main script
 def main():
     insect1 = Insect("fly", 20, 10, "flying object", 1)
     insect2 = Insect("bee", 10, 15)
@@ -49,4 +48,6 @@ def main():
     print(repr(insect1))
     print(repr(insect2))
     print(repr(insect3))
-main()
+
+if __name__ == "__main__":
+    main()
